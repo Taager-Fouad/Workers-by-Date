9043b7fdc5d390e942b107fa4a7bd4c530ea6a2a
Mohamed Mahmoud Fouad <Mohamed.Mahmoud@outhyre.co>
2026-01-07 16:14:15 +0200

    Update exclude array in script.js

--- js/script.js
+++ js/script.js
@@ -61,7 +61,7 @@ function renderTable(){
   const dateInput = document.getElementById('dateFilter').value;
   const selectedDate = formatDateForSheet(dateInput);
 
-  const exclude = ["resigned","dismissed","upl","ksa","gcc","whatsapp","tele ksa","tele-sales iraq","tele-sales","tele egy","iraq cr","iraq","eg ts","validation team","#n/a","tele iraq","egy","tele","sus","termination","support"];
+  const exclude = ["resigned","dismissed","upl","ksa","gcc","whatsapp","tele ksa","tele-sales iraq","tele-sales","tele egy","iraq cr","iraq","eg ts","validation team","#n/a","tele iraq","egy","tele","sus","termination","support","egy cr"];
 
   let filtered = allData.map(d=>{
     const shift = (d.shiftsPerDate[selectedDate] || "").replace(/"/g,'').trim();
